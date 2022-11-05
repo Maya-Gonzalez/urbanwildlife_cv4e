e2110e78a73bdb12a8fa0271ee84d5355e3c575a
Maya Gonzalez <mayagonz56@gmail.com>
2022-11-04 17:43:01 -0700

    readded files?

---
+++ projects/urban_classifier/dataset.py
@@ -0,0 +1,97 @@
+'''
+    2022 Benjamin Kellenberger, Tiziana Gelmi-Candusso
+'''
+
+import os
+import random
+from torch.utils.data import Dataset
+from torchvision.transforms import Compose, Resize, RandomRotation, RandomCrop, GaussianBlur, ToTensor, RandomApply, Normalize
+from PIL import Image
+import pandas as pd
+
+
+
+class CTDataset(Dataset):
+
+    def __init__(self, cfg, split, split_type='random_split2' , max_num=-1):
+        '''
+            Constructor. Here, we collect and index the dataset inputs and
+            labels.
+        '''
+        self.data_root = cfg['data_root']
+        self.split = split
+        self.split_type = cfg['split_type']
+        self.max_num = max_num
+        self.transform = Compose([              # Transforms. Here's where we could add data augmentation (see Björn's lecture on August 11).
+            Resize((cfg['image_size'])),  # For now, we just resize the images to the same dimensions...
+            #RandomRotation(degrees=cfg['image_rotation']), #random rotation with a rango of angles between -45 and 45 with a 10 angle interval
+            #RandomApply(transforms = [RandomCrop(224, 50)], p=0.15),
+            #RandomApply(transforms = [GaussianBlur(kernel_size= (51), sigma = (1,2))], p=0.05),
+            #nop-RandomGrayscale(), #some pictures on grayscale #this was good for birds not small mammals
+            #iaa.Sometimes(0.25, )
+            #functional.adjust_hue(image,hue_factor=0.3)
+            #GaussianBlur(kernel_size=(51, 91), sigma=2), #blur some images with a sigma of 1 and 3
+            ToTensor(),
+            #Normalize(mean = [0.485, 0.456, 0.406], std =  [0.229, 0.224, 0.225])  #normalize to speed up computations
+                          # ...and convert them to torch.Tensor.
+        ]) 
+        
+        # index data into dict
+        data_dict = {}
+        #dict categories
+        ##the following will need to be transformed into a in script mapping dictionay when adding more classes for UWIN
+        cat_csv = pd.read_csv(os.path.join(self.data_root, 'categories.csv')) #this could go into the cfg file
+        species_idx = cat_csv['class'].to_list()
+        species = cat_csv['description'].to_list()
+        self.species_to_index_mapping = dict(zip(species, species_idx))
+
+        #load the train file
+        f = open(os.path.join(self.data_root, self.split_type.lower(), self.split.lower()+'.txt'), 'r') 
+        lines = f.readlines() # load all lines
+
+        for line in lines: # loop over lines
+            file_name = line.strip().replace("\\", "/")
+            sp = os.path.split(file_name)[0]
+            # print(file_name)
+            # print(os.path.split(file_name))
+            
+            # if not, add it and assign an index
+            species_idx = self.species_to_index_mapping[sp]
+            if species_idx not in data_dict:
+                data_dict[species_idx] = []
+            data_dict[species_idx].append(file_name)
+        
+        # subsample if needed
+        self.data = []
+        for species in data_dict.keys():
+            species_list = [[i, species] for i in data_dict[species]]
+            if max_num > 0:
+                random.shuffle(species_list)
+                species_list = species_list[:min(len(species_list), max_num)]
+            self.data.extend(species_list)
+
+
+    def __len__(self):
+        '''
+            Returns the length of the dataset.
+        '''
+        return len(self.data)
+
+    
+    def __getitem__(self, idx):
+        '''
+            Returns a single data point at given idx.
+            Here's where we actually load the image.
+        '''
+        image_path, label = self.data[idx]              # see line 57 above where we added these two items to the self.data list
+
+        # load image
+        image_path = os.path.join(self.data_root, 'crops', image_path)
+        img = Image.open(image_path).convert('RGB')     # the ".convert" makes sure we always get three bands in Red, Green, Blue order
+
+        # transform: see lines 31ff above where we define our transformations
+        img_tensor = self.transform(img)
+
+        return img_tensor, label, image_path
+
+# %%
---
+++ projects/urban_classifier/evaluate.py
@@ -0,0 +1,219 @@
+import yaml
+import torch
+import scipy
+import numpy as np
+import argparse
+import os
+from glob import glob
+from tqdm import tqdm
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score, plot_confusion_matrix, average_precision_score, precision_recall_curve
+from sklearn.metrics import precision_score, recall_score, f1_score, PrecisionRecallDisplay
+from sklearn.preprocessing import label_binarize
+import matplotlib.pyplot as plt
+from train import create_dataloader, load_model 
+from tqdm import trange
+from PIL import Image
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.optim import SGD
+
+# let's import our own classes and functions!
+from util import init_seed
+from dataset import CTDataset
+from model import CustomResNet18
+from torch.utils.tensorboard import SummaryWriter 
+
+def predict(cfg, dataLoader, model, device):
+
+    model.to(device)
+    model.eval()
+
+    with torch.no_grad(): # no gradients needed for prediction
+        predictions = [] ## predictions as tensor probabilites
+        true_labels = [] ## labels as 0, 1 .. (classes)
+        predicted_labels = [] ## labels as 0, 1 .. (classes)
+        confidences = [] ## soft max of probabilities 
+        ##### may need to adjust this in the dataloader for the sequence:
+        ### this will evaluate on each batch of data (usually 64)
+        #IPython.embed()
+        #print(len(dataLoader)) ## number of total divisions n/batchsize
+        for idx, (data, label, image_path) in enumerate(tqdm(dataLoader)): 
+                #print(idx)
+            true_label = label.numpy()
+            true_labels.extend(true_label)
+
+            data = data.to(device)
+
+            prediction = model(data) ## the full probabilty
+            predictions.append(prediction.cpu())
+            #print(prediction.shape) ## it is going to be [batch size #num_classes]
+            
+            ## predictions
+            predict_label = torch.argmax(prediction.cpu(), dim=1).numpy() ## the label
+            predicted_labels.extend(predict_label)
+            #print(predict_label)
+
+            confidence = torch.nn.Softmax(dim=1)(prediction).cpu().numpy()
+            confidences.append(confidence)
+
+
+    true_labels = np.array(true_labels)
+    #print(true_labels)
+   # print(len(true_labels))
+    predicted_labels = np.array(predicted_labels)
+    #print(predicted_labels)
+    #print(len(predicted_labels))
+    #### this should be full dataset as a dataframe
+    #results = pd.DataFrame({"true_labels": true_labels, "predict_label":predicted_labels}) #"confidence":confidence
+
+    return true_labels, predicted_labels, confidences
+
+def save_confusion_matrix(true_labels, predicted_labels, cfg, args, epoch='200', split='train', split_type='random', log_dir = "latest_exp", labels=None):
+    # make figures folder if not there
+
+    os.makedirs('figs', exist_ok=True)
+
+    confmatrix = confusion_matrix(true_labels, predicted_labels, normalize = 'true')
+    disp = ConfusionMatrixDisplay(confmatrix, display_labels=labels)
+    disp.plot(values_format = '.1f')
+    #plt.show()
+    plt.savefig(f'figs/{log_dir}/{split_type}/{split}/confusion_matrix_epoch_{epoch}.png', facecolor="white")
+    
+       ## took out epoch)
+    return confmatrix
+
+def generate_results(data_loader, split, cfg, model, epoch, device, args):
+
+    split_type = cfg['split_type']
+    log_dir = cfg['log_dir']
+
+    #predict
+    true_labels, predicted_labels, confidence = predict(cfg, data_loader, model, device)
+
+    #generate function for running results with true_labels, predicted_labels, confidence as input variables
+    # legend (species names in order)
+    species_available = np.unique(true_labels).tolist()
+    species_available.sort()
+    mapping_inv = dict([v,k] for k,v in data_loader.dataset.species_to_index_mapping.items())
+    legend = np.array([mapping_inv[s] for s in species_available])
+
+    # get accuracy score
+    ### this is just a way to get two decimal places 
+    acc = accuracy_score(true_labels, predicted_labels)
+    print("Accuracy of model is {:0.2f}".format(acc))
+
+    # confusion matrix
+    os.makedirs(f'figs/{log_dir}/{split_type}/{split}/prec_rec', exist_ok=True)
+    confmatrix = save_confusion_matrix(true_labels, predicted_labels, cfg, args, epoch = epoch, split = split, split_type=split_type, log_dir = log_dir, labels=legend)
+    print("confusion matrix saved")
+
+            ###evaluation metrics:
+    # For each class
+    precision = dict()
+    recall = dict()
+    average_precision = dict()
+
+    confidence = np.concatenate(confidence, 0)
+    for i in range(confidence.shape[1]):
+        y_true = true_labels == i
+        precision[i], recall[i], _ = precision_recall_curve(y_true, confidence[:, i])
+        average_precision[i] = average_precision_score(y_true, confidence[:, i])
+
+        display = PrecisionRecallDisplay(recall=recall[i],
+        precision=precision[i],
+        average_precision=average_precision[i],
+        )
+        display.plot()
+        _ = display.ax_.set_title(f"Prec-Rec ep. {epoch}, species {mapping_inv.get(i, i)}")
+        plt.savefig(f'figs/{log_dir}/{split_type}/{split}/prec_rec/epoch_{epoch}_{mapping_inv.get(i, i)}.png', facecolor="white")
+    
+    # for i in range(confidence.shape[0]): #for every image
+    #     missclass = true_labels[i] != predicted_labels[i]
+    #     if missclass == True:
+    #         plt.imshow(img)
+                  # plt.figure()
+            # plt.imshow(img)
+            # ax = plt.gca()
+            # ax.add_patch(Rectangle(
+            #           (u_ann['bbox'][0], u_ann['bbox'][1],),
+            #           u_ann['bbox'][2], u_ann['bbox'][3],
+            #           ec='r', fill=False
+            #         ))
+            # name1 = os.path.join(path_spfull, im_dic[u_ann['image_id']])
+            # plt.savefig(name1)
+
+    #macro_average = sklearn.metrics.average_precision_score(true_labels, predicted_labels)
+        #print("Macro average of model is {:0.2f}".format(macro_average))
+
+
+def main():
+
+    # Argument parser for command-line arguments:
+    # python ct_classifier/train.py --config configs/cfg.yaml
+    parser = argparse.ArgumentParser(description='Train deep learning model.')
+    parser.add_argument('--config', help='Path to config file', default='projects/urban_classifier/configs/cfg.yaml')
+    args = parser.parse_args()
+
+    # load config
+    print(f'Using config "{args.config}"')
+    cfg = yaml.safe_load(open(args.config, 'r'))
+
+    # init random number generator seed (set at the start)
+    init_seed(cfg.get('seed', None))
+
+    # check if GPU is available
+    device = cfg['device']
+    if device != 'cpu' and not torch.cuda.is_available():
+        print(f'WARNING: device set to "{device}" but CUDA not available; falling back to CPU...')
+        cfg['device'] = 'cpu'
+
+    # initialize Tensorboard
+    tbWriter = SummaryWriter(log_dir=cfg['log_dir'])
+    os.makedirs(cfg['log_dir'], exist_ok=True)
+
+    # initialize data loaders for training and validation set
+    dl_train = create_dataloader(cfg, split='train')
+    dl_val = create_dataloader(cfg, split='val')
+    dl_test = create_dataloader(cfg, split='test')
+
+    # initialize model
+    model, epoch = load_model(cfg)
+
+    generate_results(data_loader=dl_train, split='train', cfg = cfg, model=model, epoch=epoch, device =device, args = args)
+    generate_results(data_loader=dl_val, split='val', cfg = cfg, model=model, epoch=epoch, device = device, args = args)
+    generate_results(data_loader=dl_test, split='test', cfg = cfg, model=model, epoch=epoch, device=device, args = args)
+
+
+    ######################### put this all in a function ###############
+    # #this must categorical
+    # get precision score
+    ### this is just a way to get two decimal places 
+    #precision = precision_score(true_labels, predicted_labels)
+    #print("Precision of model is {:0.2f}".format(precision))
+
+    # get recall score
+    ### this is just a way to get two decimal places 
+    #recall = recall_score(true_labels, predicted_labels)
+    #print("Recall of model is {:0.2f}".format(recall))
+
+    # get recall score
+    ### this is just a way to get two decimal places 
+    #F1score = f1_score(true_labels, predicted_labels)
+    #print("F1score of model is {:0.2f}".format(F1score))
+
+#top-k accuracy
+# class average accuracy - average precision
+#sklearn.classifier.metrics
+#classifier report
+
+if __name__ == '__main__':
+    # This block only gets executed if you call the "train.py" script directly
+    # (i.e., "python ct_classifier/train.py").
+    main()
+    
+    
+
+
+
